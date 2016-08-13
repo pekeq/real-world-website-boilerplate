1ab30f16ebf5b1deb9f3eaec32b836c5cef9a0af
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-08-13 20:51:37 +0900

    add #1

---
+++ scripts/html.js
@@ -0,0 +1,44 @@
+'use strict';
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+const pug = require('pug');
+const chokidar = require('chokidar');
+
+const servePath = process.env.npm_package_config_serve_path;
+const argv = require('minimist')(process.argv.slice(2));
+const uniq = array => Array.from(new Set(array));
+
+[{
+  src: 'pc',
+  dest: '.'
+}, {
+  src: 'sp',
+  dest: 'sp'
+}].forEach(env => {
+  const srcDir = path.join('src', env.src, 'html');
+  const destDir = path.join('dist', servePath, env.dest);
+  const render = () => {
+    const files = glob.sync(`${srcDir}/**/*.pug`);
+    const directories = uniq(
+      files.map(file => path.dirname(file).replace(srcDir, destDir))
+    );
+    directories.forEach(dir => mkdirp.sync(dir));
+    files.forEach(file => {
+      const filename = file.replace(srcDir, destDir).replace(/\.pug$/, '.html');
+      const html = pug.renderFile(file, {
+        pretty: argv.pretty
+      });
+      fs.writeFileSync(filename, html);
+    });
+  };
+
+  if (!argv.watch) {
+    render();
+  } else {
+    chokidar.watch(srcDir)
+    .on('add', render)
+    .on('change', render);
+  }
+});
