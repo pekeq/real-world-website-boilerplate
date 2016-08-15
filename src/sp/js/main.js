fd92301d719c24db2ed8b9ec75ed9aebd5b5bcff
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-08-15 15:52:23 +0900

    add link to shared js

--- src/sp/js/main.js
+++ src/sp/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 require('core-js');
-require('../../shared/velocity-easings');
+require('shared-js/velocity-easings');
 const ScrollMagic = require('scrollmagic');
-const {$} = require('../../shared/js/utils');
+const {$} = require('shared-js/utils');
 
 const controller = new ScrollMagic.Controller();
