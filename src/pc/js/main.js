0e34b2a0024ebe4dd0b6ff2bdfe988dfe1c0cb23
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-08-15 17:24:55 +0900

    add main function

--- src/pc/js/main.js
+++ src/pc/js/main.js
@@ -1,7 +1,13 @@
 'use strict';
 require('core-js');
 require('shared-js/velocity-easings');
+const Velocity = require('velocity-animate');
 const ScrollMagic = require('scrollmagic');
 const {$} = require('shared-js/utils');
 
 const controller = new ScrollMagic.Controller();
+
+const main = () => {
+};
+
+document.addEventListener('DOMContentLoaded', main);
