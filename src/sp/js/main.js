254fa427bd1dfda678fb1d30c31fdf085f3df58e
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-08-17 18:31:38 +0900

    clean

--- src/sp/js/main.js
+++ src/sp/js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 require('core-js');
 require('shared-js/velocity-easings');
+
 const Velocity = require('velocity-animate');
 const ScrollMagic = require('scrollmagic');
 const {$} = require('shared-js/utils');
-
 const controller = new ScrollMagic.Controller();
 
 const main = () => {
