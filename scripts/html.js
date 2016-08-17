5dfe3420b1e46653ce4f3887d2c9cbde7dc98ede
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-08-17 18:04:48 +0900

    simplify html task

--- scripts/html.js
+++ scripts/html.js
@@ -1,67 +1,49 @@
 'use strict';
-const values = require('object.values');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const glob = require('glob');
 const mkdirp = require('mkdirp');
-const debounce = require('lodash.debounce');
 const pug = require('pug');
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
 
-const uniq = array => Array.from(new Set(array));
+const {_: [srcDir, destDir], watch} = require('minimist')(process.argv.slice(2));
 
 const serveDir = process.env.npm_package_config_serve_dir;
-const argv = require('minimist')(process.argv.slice(2));
-const envs = {
-  pc: {
-    src: 'pc',
-    dest: '.'
-  },
-  sp: {
-    src: 'sp',
-    dest: 'sp'
-  }
+const root = {
+  pc: `/${serveDir}/`,
+  sp: `/${serveDir}/sp/`
 };
 
-values(envs).forEach(env => {
-  const envSrcDir = path.join('src', env.src, 'html');
-  const envDestDir = path.join('dist', serveDir, env.dest);
-  const render = debounce(() => {
-    const srcFiles = glob.sync(`${envSrcDir}/**/*.pug`, {
-      ignore: `${envSrcDir}/partial/**`
-    });
-    const destDirs = uniq(
-      srcFiles.map(file => path.dirname(file).replace(envSrcDir, envDestDir))
-    );
-    const metadata = require('../src/metadata.json');
-    destDirs.forEach(dir => mkdirp.sync(dir));
-    srcFiles.forEach(srcFile => {
-      const destFileName = srcFile
-      .replace(envSrcDir, envDestDir)
-      .replace(/\.pug$/, '.html');
-      const pagePath = srcFile
-      .replace(`${envSrcDir}/`, '')
-      .replace(/\.pug$/, '.html')
-      .replace(/\/?index\.html$/, '');
-      const options = Object.assign({}, metadata, {
-        pretty: true,
-        root: {
-          pc: `/${path.join(serveDir, envs.pc.dest)}/`,
-          sp: `/${path.join(serveDir, envs.sp.dest)}/`
-        },
-        currentPath: pagePath
-      });
-      const html = pug.renderFile(srcFile, options);
-      fs.writeFileSync(destFileName, html);
-    });
-    console.log('rendered html');
-  }, 300);
+const render = () => {
+  const renderFiles = glob.sync(path.join(srcDir, '**/*.pug'), {
+    ignore: path.join(srcDir, 'partial/**')
+  });
+  const metadata = require('../src/metadata.json');
 
-  if (!argv.watch) {
-    render();
-  } else {
-    chokidar.watch([envSrcDir, 'src/metadata.json'])
-    .on('add', render)
-    .on('change', render);
-  }
-});
+  renderFiles.forEach(file => {
+    const filePath = path.relative(srcDir, file).replace(/\.pug$/, '.html');
+    const destPath = path.join(destDir, filePath);
+    const pagePath = filePath.replace(/\/?index.\.html$/, '');
+    const options = Object.assign({}, metadata, {
+      pretty: true,
+      root,
+      currentPath: pagePath
+    });
+    const html = pug.renderFile(file, options);
+    mkdirp.sync(path.dirname(destPath));
+    fs.writeFileSync(destPath, html);
+  });
+
+  console.log('rendered html');
+}
+
+if (watch) {
+  const deboucnedRender = debounce(render, 300);
+
+  chokidar.watch([srcDir, 'src/metadata.json'])
+  .on('add', deboucnedRender)
+  .on('change', deboucnedRender);
+} else {
+  render();
+}
