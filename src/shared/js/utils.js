4a65ddae01bcdc821efc448bdee2c6867b8ebc33
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-08-19 17:33:11 +0900

    add util

--- src/shared/js/utils.js
+++ src/shared/js/utils.js
@@ -2,8 +2,10 @@
 
 const $ = (selector, context = document) => context.querySelector(selector);
 const $$ = (selector, context = document) => [...context.querySelectorAll(selector)];
+const wait = (delay = 0) => new Promise(done => setTimeout(done, delay));
 
 module.exports = {
   $,
-  $$
+  $$,
+  wait
 };
