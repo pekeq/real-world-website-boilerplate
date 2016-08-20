268884260dc3fda2b871c8d097d66a608e3bd3cc
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-08-21 01:18:06 +0900

    split by sub directries

--- src/pc/js/main.js
+++ src/pc/js/main.js
@@ -1,13 +1,29 @@
 'use strict';
 require('core-js');
 require('shared-js/velocity-easings');
+const Dispatcher = require('@yuheiy/page-dispatcher');
 
-const Velocity = require('velocity-animate');
-const ScrollMagic = require('scrollmagic');
-const {$} = require('shared-js/utils');
-const controller = new ScrollMagic.Controller();
+const dispatcher = new Dispatcher();
+const home = () => {
+  const Velocity = require('velocity-animate');
+  const ScrollMagic = require('scrollmagic');
+  const {$} = require('shared-js/utils');
+  const controller = new ScrollMagic.Controller();
 
-const main = () => {
+  // do stuff...
 };
 
-document.addEventListener('DOMContentLoaded', main);
+dispatcher.on('home', home);
+
+window.ProjectNameSpace = {
+  run(...args) {
+    if (document.readyState === 'loading') {
+      document.addEventListener(
+        'DOMContentLoaded',
+        dispatcher.run.bind(dispatcher, ...args)
+      );
+    } else {
+      dispatcher.run(...args);
+    }
+  }
+}
