f726663019cc6a6b15be56d9be3f5f2e86252d2c
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-09-04 18:14:46 +0900

    rename `website.json` to `metadata.json`

--- scripts/html.js
+++ scripts/html.js
@@ -9,7 +9,7 @@ const debounce = require('lodash.debounce');
 
 const {_: [srcDir, destDir], watch} = require('minimist')(process.argv.slice(2));
 
-const siteDataPath = 'src/website.json';
+const metaDataPath = 'src/metadata.json';
 const serveDir = process.env.npm_package_config_serveDir;
 const root = {
   pc: path.join('/', serveDir, '/'),
@@ -20,13 +20,13 @@ const render = () => {
   const renderFiles = glob.sync(path.join(srcDir, '**/*.pug'), {
     ignore: path.join(srcDir, 'partial/**')
   });
-  const siteData = JSON.parse(fs.readFileSync(siteDataPath, 'utf8'));
+  const metaData = JSON.parse(fs.readFileSync(metaDataPath, 'utf8'));
 
   renderFiles.forEach(file => {
     const filePath = path.relative(srcDir, file).replace(/\.pug$/, '.html');
     const destPath = path.join(destDir, filePath);
     const pagePath = filePath.replace(/\/?index\.html$/, '');
-    const options = Object.assign({}, siteData, {
+    const options = Object.assign({}, metaData, {
       pretty: true,
       root,
       currentPath: pagePath
@@ -42,7 +42,7 @@ const render = () => {
 if (watch) {
   const deboucnedRender = debounce(render, 300);
 
-  chokidar.watch([srcDir, siteDataPath])
+  chokidar.watch([srcDir, metaDataPath])
   .on('add', deboucnedRender)
   .on('change', deboucnedRender);
 } else {
