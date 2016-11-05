e6605274e0295a95e272dec356de28c8d4e2e153
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-11-06 06:04:08 +0900

    add

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-export const wait = (delay = 0) => new Promise(done => setTimeout(done, delay))
+export const wait = (delay = 4) => new Promise(done => setTimeout(done, delay))
 
 // dom
 export const $ = (selector, context = document) => context.querySelector(selector)
