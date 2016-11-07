da123d2342c29296ea713c9e6d3da15af68170de
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-11-07 16:27:39 +0900

    add

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,3 +3,17 @@ export const wait = (delay = 4) => new Promise(done => setTimeout(done, delay))
 // dom
 export const $ = (selector, context = document) => context.querySelector(selector)
 export const $$ = (selector, context = document) => context.querySelectorAll(selector)
+
+export const interactive = new Promise(resolve => {
+  const EVENT_TYPE = 'readystatechange'
+
+  const listener = () => {
+    if (['interactive', 'complete'].includes(document.readyState)) {
+      document.removeEventListener(EVENT_TYPE, listener)
+      resolve()
+    }
+  }
+
+  document.addEventListener(EVENT_TYPE, listener)
+  listener()
+})
