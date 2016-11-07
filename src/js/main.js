625c9eab51c573346d4e024948bb2662a137e509
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-11-07 16:27:39 +0900

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 // import './velocity-easings.js'
-import {interactive} from 'document-promises'
-import {wait, $, $$} from './utils.js'
+import {wait, $, $$, interactive} from './utils.js'
 
 require('picturefill')
 const PageDispatcher = require('@yuheiy/page-dispatcher')
@@ -17,7 +16,8 @@ dispatcher.on('about', () => {
 })
 
 window.App = class App {
-  static run(...args) {
-    return interactive.then(() => dispatcher.run(...args))
+  static async run(...args) {
+    await interactive
+    return dispatcher.run(...args)
   }
 }
